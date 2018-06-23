4638bd83e901ea7669129b5a45660d2236b3e821
Luke Rickard <luke.x.rickard@gmail.com>
2018-06-23 15:55:45 +0100

    Preparing for prod deployment 3

---
+++ src/environments/version.js
@@ -0,0 +1,13 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": false,
+    "raw": "8cb681d",
+    "hash": "8cb681d",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "8cb681d",
+    "semverString": null
+};
+/* tslint:enable */
